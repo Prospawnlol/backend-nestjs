5912518e3a0ae4b39fdc54cc653badf583371108
Prospawnlol <pablojaredmosquera@gmail.com>
2024-09-13 13:44:00 -0500

    cambios para conectar back y front

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+// src/projects/projects.controller.ts
 
-@Controller('projects')
-export class ProjectsController {}
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('projects')  // Define la ruta base /projects
+export class ProjectsController {
+  @Get()  // Maneja la petición GET en /projects
+  getProjects() {
+    return { name: 'Pablo Mosquera', profileImage: 'https://robohash.org/20da7dfce73507171987385b94d8a99a?set=set2&bgset=bg1&size=400x400' };
+  }
+}
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+    @Get('polizas')
+    getData(){
+        return {message:'Hello from NestJS'};
+    }
+}
+
